3919458f9b360ea5950d63b16ea8107539ecea00
trascent <luchopodex@hotmail.com>
2022-05-02 00:57:01 -0500

    Implementación de vistas para el crud de registro de facturas

--- src/views/BillsView.vue
+++ src/views/BillsView.vue
@@ -1,14 +1,19 @@
 <template>
-  <table-form></table-form>
+  <div>
+    <toolbar-menu :principal="true"></toolbar-menu>
+    <table-form></table-form>
+  </div>
 </template>
 <script lang="js">
 import TableForm from "../components/TableForm.vue";
+import ToolbarMenu from "../components/ToolbarMenu.vue";
 import { defineComponent } from "vue";
 import axios from "axios";
 export default defineComponent({
   name: "BillsView",
   components: {
     TableForm,
+    ToolbarMenu
   },
   data() {
     const data = []
@@ -16,5 +21,25 @@ export default defineComponent({
       data,
     };
   },
+  created(){
+    console.log(this.$store.state.authenticationChecker.authentication)
+    console.log(this.$store.state.authenticationChecker.token)
+    // Verificar si está logueado
+    if(!this.$store.state.authenticationChecker.authentication){
+      this.$router.push({
+        name: "LoginPage",
+        params: {
+          message: "Usuario no autorizado, por favor vuelva a entrar.",
+        },
+        replace: true,
+      });
+    }
+  },
 });
 </script>
+<style lang="scss">
+.p-toolbar {
+  width: 90vw;
+  margin: 0;
+}
+</style>
--- src/views/EditBillView.vue
+++ src/views/EditBillView.vue
@@ -1,50 +1,83 @@
 <template>
-  <bill-form v-model="form" />
-  <Card>
-    <template #content>
-      <Button
-        label="Volver"
-        icon="pi pi-fw pi-power-off"
-        class="p-button-danger"
-        @click="back()"
-      />
-      <Button
-        label="Editar Factura"
-        icon="pi pi-fw pi-pencil"
-        class="p-button-success mr-2"
-        @click="update()"
-      />
-    </template>
-  </Card>
+  <div>
+    <toolbar-menu :principal="false"></toolbar-menu>
+    <bill-form :bill-data="form" mode="edit" :errors="errors" />
+    <Button label="Volver" class="p-button-danger" @click="back()" />
+    <Button
+      label="Guardar Cambios"
+      icon="pi pi-fw pi-pencil"
+      class="p-button-success mr-2"
+      @click="update()"
+    />
+  </div>
 </template>
 <script lang="js">
 import { defineComponent } from "vue";
+import BillForm from "../components/BillForm.vue";
 import axios from "axios";
+import Button from "primevue/button"; // Import the PrimeVue buttons.
+import "primeflex/primeflex.css"; // Import the PrimeVue layout utility library.
+import ToolbarMenu from "../components/ToolbarMenu.vue";
 export default defineComponent({
-  name: "BillsList",
+  name: "EditBill",
   components: {
+    BillForm,
+    Button,
+    ToolbarMenu
   },
   props: {
     /**
      * número de la factura
      */
-    number: {
+    id: {
       type: String,
       required: true,
     },
   },
   data() {
     return {
-      form: {},
+      errors: {},
+      form: {
+        productsList: [
+        ]
+      },
     };
   },
-  /**
-   * Obtiene los datos de la factura
-   */
   created() {
+    console.log(this.$store.state.authenticationChecker.authentication)
+    // Verificar si está logueado
+    if(!this.$store.state.authenticationChecker.authentication){
+      this.$router.push({
+        name: "LoginPage",
+        params: {
+          message: "Usuario no autorizado, por favor vuelva a entrar.",
+        },
+        replace: true,
+      });
+    }
+    /**
+    * Obtiene los datos de la factura
+    */
     const url = import.meta.env.VITE_VUE_APP_ENDPOINT_URL;
-    axios.get(url + "bills/"+ this.id + "/edit").then((response) => {
+    // Traer datos de las facturas
+    axios.get(url + "bills/"+ parseInt(this.id) + "/edit", {
+      headers: {
+        Authorization: "Bearer " + this.$store.state.authenticationChecker.token
+      }
+    }).then((response) => {
       return this.form = response.data;
+    });
+    // Traer datos de los productos
+    axios.get(url + "products", {
+      headers: {
+        Authorization: "Bearer " + this.$store.state.authenticationChecker.token
+      }
+    }).then((response) => {
+      const products = response.data;
+      products.forEach(function (element) {
+        element.quantity = 0;
+      });
+      return this.form.productsList = products;
     });
   },
   computed: {
@@ -56,8 +89,22 @@ export default defineComponent({
     update: function () {
       // Hacer solicitud HTTP
       const url = import.meta.env.VITE_VUE_APP_ENDPOINT_URL;
-      axios.put(url + "bills/" + this.number, this.form).then((response) => {
-        return response.data;
+      axios.put(url + "bills/" + parseInt(this.id), this.form, {
+      headers: {
+        Authorization: "Bearer " + this.$store.state.authenticationChecker.token
+      }
+      }).then((response) => {
+        if(response.data === "success"){
+          this.$router.push({
+            name: "ShowBill",
+            params: {
+              id: this.id,
+            },
+            replace: true,
+          });
+        }
+      }, (error) => {
+        this.errors = error.response.data
       });
     },
   },
